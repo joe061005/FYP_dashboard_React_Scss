7996e27ba36793ec4df6eacd3040ff52f6f94e38
joe061005 <18233368@life.hkbu.edu.hk>
2022-06-19 00:57:31 +0800

    create the side bar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 import './sidebar.scss'
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import LandscapeOutlinedIcon from '@mui/icons-material/LandscapeOutlined';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import ArticleOutlinedIcon from '@mui/icons-material/ArticleOutlined';
+import GroupOutlinedIcon from '@mui/icons-material/GroupOutlined';
+import MyLocationOutlinedIcon from '@mui/icons-material/MyLocationOutlined';
+import CookieOutlinedIcon from '@mui/icons-material/CookieOutlined';
+import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined';
 
 const Sidebar = () => {
   return (
@@ -12,23 +21,58 @@ const Sidebar = () => {
       <hr />
       <div className="center">
         <ul>
+          <p className="title">MAIN</p>
           <li>
+            <DashboardIcon className='icon'/>
             <span>Dashboard</span>
           </li>
+          <p className="title">DB INFORMATION</p>
           <li>
-            <span>Dashboard</span>
+            <PersonOutlineOutlinedIcon className='icon'/>
+            <span>Users</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <LandscapeOutlinedIcon className='icon'/>
+            <span>Trails</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <InfoOutlinedIcon className='icon' />
+            <span>Trail Information</span>
+          </li>
+          <li>
+            <ArticleOutlinedIcon className='icon'/>
+            <span>Forms</span>
+          </li>
+          <li>
+            <GroupOutlinedIcon className='icon'/>
+            <span>Hiking Groups</span>
+          </li>
+          <li>
+            <MyLocationOutlinedIcon className='icon'/>
+            <span>Locations</span>
+          </li>
+          <li>
+            <CookieOutlinedIcon className='icon'/>
+            <span>Sessions</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <ExitToAppOutlinedIcon className='icon'/>
+            <span>Logout</span>
           </li>
         </ul>
 
       </div>
       <div className="bottom">
-        color options
+        <div className="colorOption">
+
+        </div>
+        <div className="colorOption">
+          
+        </div>
+        <div className="colorOption">
+          
+        </div>
       </div>
 
     </div>
