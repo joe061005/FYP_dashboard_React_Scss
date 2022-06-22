78c29d2dfba0b8eeba8cc9ee560e79dcf18b4e84
joe061005 <18233368@life.hkbu.edu.hk>
2022-06-22 22:51:39 +0800

    create chart on home page

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -29,14 +29,14 @@ const Featured = () => {
             </div>
           </div>
           <div className="item">
-            <div className="itemTitle">Target</div>
+            <div className="itemTitle">Last Week</div>
             <div className="itemResult positive">
               <KeyboardArrowUpOutlinedIcon font='small' />
               <div className="resultAmount">12.4k</div>
             </div>
           </div>
           <div className="item">
-            <div className="itemTitle">Target</div>
+            <div className="itemTitle">Last Month</div>
             <div className="itemResult negative">
               <KeyboardArrowUpOutlinedIcon font='small' />
               <div className="resultAmount">12.4k</div>
